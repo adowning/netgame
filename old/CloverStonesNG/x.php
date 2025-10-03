dbe2fe5772d9f5706f6adf8c16326b007acc8772
adowning <ashdowning@gmail.com>
2025-10-03 00:24:10 -0500

    sdf

---
+++ old/CloverStonesNG/x.php
@@ -0,0 +1,69 @@
+<?php
+namespace VanguardLTE\Games\CloverStonesNG
+{
+   class Server
+    {
+
+            function get_($request, $game)
+            {
+
+                    try
+                    {
+
+                        $userId = \Auth::id();
+                        if( $userId == null )
+                        {
+                            $response = '{"responseEvent":"error","responseType":"","serverResponse":"invalid login"}';
+                            exit( $response );
+                        }
+                        $slotSettings = new SlotSettings($game, $userId);
+                        if( !$slotSettings->is_active() )
+                        {
+                            $response = '{"responseEvent":"error","responseType":"","serverResponse":"Game is disabled"}';
+                            exit( $response );
+                        }
+                        $postData = json_decode(trim(file_get_contents('php://input')), true);
+                        $result_tmp = [];
+                        if( isset($postData['gameData']) )
+                        {
+                            $postData = $postData['gameData'];
+                            $reqId = $postData['cmd'];
+                            if( !isset($postData['cmd']) )
+                            {
+                                $response = '{"responseEvent":"error","responseType":"","serverResponse":"incorrect action"}';
+                                exit( $response );
+                            }
+                        }
+                        else
+                        {
+                            $reqId = $postData['action'];
+                        }
+
+                     switch( $reqId )
+                        {
+                            case 'InitRequest':
+                                $result_tmp[0] = '{"action":"InitResponce","result":true,"sesId":"a40e5dc15a83a70f288e421fbcfc6de8","data":{"id":16183084}}';
+                                exit( $result_tmp[0] );
+                                break;
+
+                                                    case 'BalanceRequest':
+                                $result_tmp[] = '{"action":"BalanceResponse","result":"true","sesId":"10000214325","data":{"entries":"0.00","totalAmount":"' . $slotSettings->GetBalance() . '","currency":"' . $slotSettings->slotCurrency . '"}}';
+                                break;
+                            case 'FreeSpinRequest':
+                            case 'SpinRequest':
+                            $slotSettings->SetGameData($slotSettings->slotId . 'initialSymbols', $symb);
+                                    $result_tmp[] = '{"action":"SpinResponse","result":"true","sesId":"10000217909","data":{"state":"' . $gameState . '"' . $winString . ',' . $resultStages . '"spinResult":{"type":"SpinResult","rows":[' . $symb . ']}}}';
+                                }
+                                break;
+
+                                            $response = implode('------', $result_tmp);
+                        $slotSettings->SaveGameData();
+                        $slotSettings->SaveGameDataStatic();
+                        echo ':::' . $response;
+
+
+                            }
+                        }
+                        }
+                    }
+                        }
